87498538462a46eadeaae19715bcb1f836628547
MihaelaWBS <mihaela.mocanu.ux@gmail.com>
2023-11-23 16:38:57 +0100

    almost finished level 1, just need to update colors

--- TriviaProject/src/App.jsx
+++ TriviaProject/src/App.jsx
@@ -2,19 +2,29 @@ import { useState } from "react";
 import questions from "./questions";
 import Questions from "./components/Questions";
 import "./App.css";
+import Results from "./components/Results";
 
 function App() {
 	const [quizQuestions, setQuizQuestions] = useState(questions);
 	const [selectedAnswers, setSelectedAnswers] = useState({});
+	const [quizOver, setQuizOver] = useState(false);
 
 	const handleAnswerSelect = (questionId, answer) => {
 		setSelectedAnswers({...selectedAnswers, [questionId]: answer});
+		if (Object.keys(selectedAnswers).length === quizQuestions.length - 1) {
+            setQuizOver(true); 
+        }
 
 	};
-		
 
-		
+	const restartQuiz = () => {
+        setSelectedAnswers({});
+        setQuizOver(false);
+    };
 
+    if (quizOver) {
+        return <Results questions={quizQuestions} answers={selectedAnswers} onRestart={restartQuiz} />;
+    }
  
 	return (
 		<>
